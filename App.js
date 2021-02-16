e686e3e0f44a0732af9cebf3b6493cf3c6b83672
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-02-16 20:56:32 +0900

    [UPDATE]Weather component

--- App.js
+++ App.js
@@ -3,19 +3,25 @@ import Loading from "./components/Loading"
 import * as Location from "expo-location"
 import { BASE_URL, API_KEY } from "./config/config.json"
 import axios from "axios"
-import { View, Text } from "react-native"
+import { View, Text, StatusBar } from "react-native"
 import Weather from "./Weather"
 
 export default function App() {
     const [isLoading, setIsLoading] = useState(true)
     const [temp, setTemp] = useState()
-
+    const [condition, setCondition] = useState("Snow")
     const getWeather = async (latitude, longitude) => {
         try {
-            const { data } = await axios.get(
+            const {
+                data: {
+                    main: { temp },
+                    weather,
+                },
+            } = await axios.get(
                 `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
             )
-            setTemp(data.main.temp)
+            setTemp(temp)
+            setCondition(weather[0].main)
             // return data
         } catch (error) {
             console.log(error)
@@ -41,5 +47,12 @@ export default function App() {
         getLocation()
     }, [])
 
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />
+    return isLoading ? (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <Loading />
+        </>
+    ) : (
+        <Weather temp={Math.round(temp)} condition={condition} />
+    )
 }
--- Weather.js
+++ Weather.js
@@ -1,17 +1,89 @@
 import React from "react"
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, StatusBar } from "react-native"
 import PropTypes from "prop-types"
+import { MaterialCommunityIcons, Ionicons, Feather } from "@expo/vector-icons"
+import { LinearGradient } from "expo-linear-gradient"
 
-function Weather({ temp }) {
+const weatherOptions = {
+    Thunderstorm: {
+        iconName: "weather-lightning",
+        backColor: ["#373B44", "#4286f4"],
+        title: "천둥이 당신의 집을 습격합니다!",
+        subTitle: "",
+    },
+    Drizzle: {
+        iconName: "weather-hail",
+        backColor: ["#89F7FE", "#66A6FF"],
+        title: "이슬이 똑똑",
+        subTitle: "생각이 많아지는 날씨네요",
+    },
+    Rain: {
+        iconName: "weather-rainy",
+        backColor: ["#00C6FB", "#005BEA"],
+        title: "비가온다!!",
+        subTitle: "빗길 차 운전은 항상 조심!",
+    },
+    Snow: {
+        iconName: "weather-snowy",
+        backColor: ["#7DE2FC", "#B9B6E5"],
+        title: "우와! 눈이 내려요",
+        subTitle: "나랑 눈사람 만들래?",
+    },
+    Atmosphere: {
+        iconName: "weather-hail",
+        backColor: ["#89F7FE", "#66A6FF"],
+        title: "",
+        subTitle: "",
+    },
+    Clear: {
+        iconName: "weather-sunny",
+        backColor: ["#FF7300", "#FEF253"],
+        title: "맑아요!",
+        subTitle: "밖에 나가서 놀다오는건 어때요?",
+    },
+    Clouds: {
+        iconName: "weather-cloudy",
+        backColor: ["#4c669f", "#3b5998", "#192f6a"],
+        title: "구름이 많아요...",
+        subTitle: "센치하네요",
+    },
+}
+
+function Weather({ temp, condition }) {
     return (
-        <View style={styles.container}>
-            <Text>{temp}</Text>
-        </View>
+        <LinearGradient
+            // Button Linear Gradient
+            colors={weatherOptions[condition].backColor}
+            style={styles.container}
+        >
+            <StatusBar barStyle="light-content" />
+            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons
+                    name={weatherOptions[condition].iconName}
+                    size={96}
+                    color="white"
+                />
+                <Text style={styles.temp}>{temp}</Text>
+            </View>
+            <View style={styles.halfContainer}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subTitle}>{weatherOptions[condition].subTitle}</Text>
+            </View>
+        </LinearGradient>
     )
 }
 
 Weather.propTypes = {
     temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+    ]).isRequired,
 }
 
 const styles = StyleSheet.create({
@@ -19,6 +91,26 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+    },
+    halfContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    temp: {
+        fontSize: 36,
+        color: "white",
+    },
+    title: {
+        fontSize: 44,
+        color: "white",
+        fontWeight: "300",
+        marginBottom: 10,
+    },
+    subTitle: {
+        fontWeight: "bold",
+        color: "white",
+        fontSize: 24,
     },
 })
 
