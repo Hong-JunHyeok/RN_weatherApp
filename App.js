694a82c13ec5e4310e3c54451a69e3d47c53abbb
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-02-16 18:48:01 +0900

    [ADD]state loading rendering

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Loading from "./components/Loading"
 import * as Location from "expo-location"
 
 export default function App() {
+    const [isLoading, setIsLoading] = useState(true)
     getLocation = async () => {
         try {
             await Location.requestPermissionsAsync()
             const {
                 coords: { latitude, longitude },
             } = await Location.getCurrentPositionAsync({})
-            console.log(latitude, longitude)
+            setIsLoading(false)
         } catch (error) {
             console.log(error)
+            setIsLoading(false)
         }
     }
 
     useEffect(() => {
         getLocation()
     }, [])
-    return <Loading />
+
+    return isLoading ? <Loading /> : <></>
 }
