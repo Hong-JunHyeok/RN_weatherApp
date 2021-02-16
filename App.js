0a7e251dad4c881009f682e4929e2b8602e97752
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-02-16 17:37:11 +0900

    [ADD]first build

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { Alert, StyleSheet, Text, View } from "react-native"
+
+const onClick = () => {
+    console.log("Hello")
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header} onTouchStart={onClick} />
+            <Text style={styles.text}>My first App build</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    header: {
+        width: 100,
+        height: 100,
+        backgroundColor: "black",
+        borderRadius: 70,
+    },
+    text: {
+        fontSize: 40,
+    },
+})
