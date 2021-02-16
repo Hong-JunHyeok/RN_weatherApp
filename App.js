009b5423c3e1f3714540bd20c15600ef06c4c382
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-02-16 18:36:10 +0900

    [ADD]expo location and get Position

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import { StatusBar } from "expo-status-bar"
-import React from "react"
-import { Alert, StyleSheet, Text, View } from "react-native"
+import React, { useEffect } from "react"
 import Loading from "./components/Loading"
+import * as Location from "expo-location"
 
 export default function App() {
+    getLocation = async () => {
+        try {
+            await Location.requestPermissionsAsync()
+            const {
+                coords: { latitude, longitude },
+            } = await Location.getCurrentPositionAsync({})
+            console.log(latitude, longitude)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getLocation()
+    }, [])
     return <Loading />
 }
