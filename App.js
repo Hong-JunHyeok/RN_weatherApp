6a603e06776163fa797bdda738c624e60009dd7e
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-02-16 18:00:02 +0900

    [ADD] flexbox styling

--- App.js
+++ App.js
@@ -2,33 +2,42 @@ import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { Alert, StyleSheet, Text, View } from "react-native"
 
-const onClick = () => {
-    console.log("Hello")
-}
-
 export default function App() {
     return (
         <View style={styles.container}>
-            <View style={styles.header} onTouchStart={onClick} />
-            <Text style={styles.text}>My first App build</Text>
+            <View style={styles.greenView}>
+                <Text style={styles.text}>Hello</Text>
+            </View>
+            <View style={styles.blueView}>
+                <Text style={styles.text}>World</Text>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "white",
+        display: "flex",
         justifyContent: "center",
     },
-    header: {
-        width: 100,
-        height: 100,
-        backgroundColor: "black",
-        borderRadius: 70,
+    greenView: {
+        backgroundColor: "green",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    blueView: {
+        backgroundColor: "blue",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
     },
     text: {
+        color: "white",
+        fontWeight: "bold",
         fontSize: 40,
     },
 })
