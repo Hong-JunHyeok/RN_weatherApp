a607a2a958c9a2e970045fbf21a6081685e47b73
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-02-16 19:24:00 +0900

    [ADD]axios success

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import React, { useEffect, useState } from "react"
 import Loading from "./components/Loading"
 import * as Location from "expo-location"
+import { BASE_URL, API_KEY } from "./config/config.json"
+import axios from "axios"
 
 export default function App() {
     const [isLoading, setIsLoading] = useState(true)
-    getLocation = async () => {
+
+    const getWeather = async (latitude, longitude) => {
+        try {
+            const { data } = await axios.get(
+                `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+            )
+            console.log(data)
+            // return data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const getLocation = async () => {
         try {
             await Location.requestPermissionsAsync()
             const {
                 coords: { latitude, longitude },
             } = await Location.getCurrentPositionAsync({})
+
+            await getWeather(latitude, longitude)
             setIsLoading(false)
         } catch (error) {
             console.log(error)
--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,19 @@
 # RN Weather App
+
+# API 사용 방식
+
+```javascript
+Request => `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+
+Location.getCurrentPositionAsync()로 가져온 데이터 중 latitude, longitude를
+lat , lon 쿼리에 전달해주면 됩니다.
+
+API 제공 홈페이지에서 MY API KEY 에 접근하시면 내 API 키를 가져올 수 있습니다.
+
+```
+
+# 기술 스택
+
+![image](https://img.shields.io/badge/-React%20Native-blue)
+![image](https://img.shields.io/badge/-Expo-black)
+![image](https://img.shields.io/badge/-Axios-lightgrey)
